5c58e58735e48552948d55095af43f4db2d0f096
alp <sunnatbey1@gmail.com>
2024-03-10 21:22:00 +0500

    textlar style authgacha yozildi

---
+++ lib/src/core/style/text_style.dart
@@ -0,0 +1,13 @@
+
+import 'package:barcodbek/src/core/style/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTextStyle{
+
+  static final textStyle1 =  GoogleFonts.signika(textStyle: TextStyle(color: AppColord.c_FFFFFF,fontSize: 20.sp,fontWeight: FontWeight.w800));
+  static final textStyle2 =  GoogleFonts.signika(textStyle: TextStyle(color: AppColord.c_000000,fontSize: 25.sp,fontWeight: FontWeight.w600));
+  static final textStyle4 =  GoogleFonts.signika(textStyle:const TextStyle(color: AppColord.c_C8C8C8,fontSize: 17,fontWeight: FontWeight.w700));
+  static final textStyle3 =  GoogleFonts.lato(textStyle: TextStyle(color: AppColord.c_C8C8C8,fontSize: 15.sp,fontWeight: FontWeight.w400));
+}
