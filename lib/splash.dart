1e9d6e77170938bb0265cbc9bcd6cd71a4192eeb
davlat <davlatsalimov936@gmail.com>
2024-03-10 05:37:14 -0700

    Alpomish - 2 commit failar kk qildi

--- lib/splash.dart
+++ lib/splash.dart
@@ -1,10 +1,24 @@
+import 'package:barcodbek/src/future/home/view/pages/home_Page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class SplashPages extends StatelessWidget {
+class SplashPages extends StatefulWidget {
   const SplashPages({super.key});
 
+  @override
+  State<SplashPages> createState() => _SplashPagesState();
+}
+
+class _SplashPagesState extends State<SplashPages> {
+
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 2),(){
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const HomePage()));
+    });
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return   Scaffold(
