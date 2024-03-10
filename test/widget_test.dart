a0cbba5c3a5268a23b7ee431dfcd93cdb636f502
alp <sunnatbey1@gmail.com>
2024-03-10 20:33:03 +0500

    splash va onboarding logo color

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,7 +3,7 @@
 // To perform an interaction with a widget in your test, use the WidgetTester
 // utility in the flutter_test package. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
+// tree, read text, and verify that the values of widget properties are
 
 import 'package:barcodbek/app.dart';
 import 'package:flutter/material.dart';
